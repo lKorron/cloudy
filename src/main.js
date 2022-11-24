6ab36a6ed40637ed0ad7ea08ba17b68b6d3b0658
Korron <lkorron@yandex.ru>
2022-11-24 23:33:49 +0300

    sign up validation done

--- src/main.js
+++ src/main.js
@@ -6,9 +6,27 @@ import { initializeApp } from "firebase/app";
 import { defineRule } from "vee-validate";
 import { required, email, is } from "@vee-validate/rules";
 
-defineRule("required", required);
+const requiredWrapper = (value) => {
+  return required(value) || "This field is required";
+};
+
+const isPassword = (value, params) => {
+  return is(value, params) || "Passwords must be the same";
+};
+
+defineRule("required", requiredWrapper);
 defineRule("email", email);
-defineRule("is", is);
+defineRule("isPassword", isPassword);
+
+defineRule("min", (value, [limit]) => {
+  if (!value || !value.length) {
+    return true;
+  }
+  if (value.length < limit) {
+    return `Password must be at least ${limit} characters`;
+  }
+  return true;
+});
 
 const firebaseConfig = {
   apiKey: "AIzaSyAtkvfPAph6V8TFIPw3z83Wa9aq8WeC_OE",
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -7,54 +7,78 @@
         v-slot="{ meta }"
         class="text-xs min-[350px]:text-base max-w-sm mx-auto p-2 md:max-w-md"
       >
-        <div class="flex justify-between">
-          <label for="login">Email</label>
-          <Field
-            v-model="email"
-            rules="required|email"
-            placeholder="example@gmail.com"
-            type="email"
-            name="email"
-            id="email"
-            class="pl-1 border-b border-solid border-gray-400 mb-5 focus:outline-none"
-          />
+        <div class="mb-5">
+          <div class="flex justify-between items-center">
+            <label for="login">Email</label>
+            <Field
+              v-model="email"
+              rules="required|email"
+              placeholder="example@gmail.com"
+              type="email"
+              name="email"
+              id="email"
+              class="pl-1 border-b border-solid border-gray-400 focus:outline-none"
+            />
+          </div>
+          <ErrorMessage name="email" v-slot="{ message }">
+            <div class="text-right text-red-500">{{ message }}</div>
+          </ErrorMessage>
         </div>
-        <div class="flex justify-between">
-          <label for="login">Username</label>
-          <Field
-            v-model="login"
-            rules="required"
-            placeholder="Create an username"
-            type="text"
-            name="login"
-            id="login"
-            class="pl-1 border-b border-solid border-gray-400 mb-5 focus:outline-none"
-          />
+
+        <div class="mb-5">
+          <div class="flex justify-between items-center">
+            <label for="login">Username</label>
+            <Field
+              v-model="login"
+              rules="required"
+              placeholder="Create an username"
+              type="text"
+              name="login"
+              id="login"
+              class="pl-1 border-b border-solid border-gray-400 focus:outline-none"
+            />
+          </div>
+          <ErrorMessage name="login" v-slot="{ message }">
+            <div class="text-right text-red-500">{{ message }}</div>
+          </ErrorMessage>
         </div>
-        <div class="flex justify-between">
-          <label for="password" class="mr-5">Password</label>
-          <Field
-            v-model="password"
-            :rules="{ required: true, is: confirmPassword }"
-            placeholder="Enter your password"
-            type="password"
-            name="password"
-            id="password"
-            class="pl-1 border-b border-solid border-gray-400 mb-5 focus:outline-none"
-          />
+
+        <div class="mb-5">
+          <div class="flex justify-between items-center">
+            <label for="password" class="mr-5">Password</label>
+            <Field
+              v-model="password"
+              :rules="{ required: true, min: 6 }"
+              placeholder="Enter your password"
+              type="password"
+              name="password"
+              id="password"
+              class="pl-1 border-b border-solid border-gray-400 focus:outline-none"
+            />
+          </div>
+          <ErrorMessage name="password" v-slot="{ message }">
+            <div class="text-right text-red-500">{{ message }}</div>
+          </ErrorMessage>
         </div>
-        <div class="flex justify-between md:text-md">
-          <label for="password">Repeat password</label>
-          <Field
-            v-model="confirmPassword"
-            :rules="{ required: true, is: password }"
-            placeholder="Repeat your password"
-            type="password"
-            name="password-repeat"
-            id="password-repeat"
-            class="pl-1 border-b border-solid border-gray-400 mb-5 focus:outline-none"
-          />
+
+        <div class="mb-5">
+          <div class="flex justify-between items-center">
+            <label for="password">Repeat password</label>
+            <Field
+              v-model="confirmPassword"
+              :rules="{ required: true, isPassword: password }"
+              placeholder="Repeat your password"
+              type="password"
+              name="password-repeat"
+              id="password-repeat"
+              class="pl-1 border-b border-solid border-gray-400 focus:outline-none"
+            />
+          </div>
+          <ErrorMessage name="password-repeat" v-slot="{ message }">
+            <div class="text-right text-red-500">{{ message }}</div>
+          </ErrorMessage>
         </div>
+
         <button
           class="hover:scale-105 transition-transform border-none bg-sky-200 pl-3 pr-3 rounded-md mb-2 cursor-pointer"
           :class="{
@@ -71,7 +95,7 @@
 <script setup>
 import { ref } from "vue";
 import ContentPanel from "../components/ContentPanel.vue";
-import { Form, Field, useForm } from "vee-validate";
+import { Form, Field, ErrorMessage } from "vee-validate";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 const email = ref();
