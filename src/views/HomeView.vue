487fe734df35af3abcd05313dcc4bea0bba58399
Korron <lkorron@yandex.ru>
2023-02-13 12:47:28 +0300

    main view bug fix

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,5 @@
 <template>
-  <div class="container">
-    <div class="relative inline-block">
-      <img
-        src="../assets/cloud.png"
-        alt="cloud"
-        class="h-10 absolute left-[-45px] top-3"
-      />
-      <h1 class="mx-auto font-bold text-[30px] mt-3">Cloudy</h1>
-    </div>
-  </div>
+  <div></div>
 </template>
 
 <script setup>
