f068573237086d71a84a72a76527e1ee26531b23
Korron <lkorron@yandex.ru>
2023-03-08 16:57:57 +0300

    deletion.js

---
+++ src/composables/deletion.js
@@ -0,0 +1,55 @@
+import { ref, listAll, deleteObject } from "firebase/storage";
+
+export function useDeletion(storage, fileList) {
+  const deleteFromStorage = (filePath, fileType) => {
+    switch (fileType) {
+      case "document":
+        deleteFileFromStorage(filePath);
+        break;
+      case "folder":
+        deleteFolderFromStorage(filePath);
+        break;
+
+      default:
+        break;
+    }
+  };
+
+  const deleteFileFromStorage = (filePath) => {
+    const fileRef = ref(storage, filePath);
+
+    deleteObject(fileRef)
+      .then(() => {
+        removeFromList(fileList, filePath, "document");
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const deleteFolderFromStorage = (folderPath) => {
+    const folderRef = ref(storage, folderPath);
+
+    listAll(folderRef)
+      .then((dir) => {
+        dir.items.forEach((fileRef) => {
+          deleteFileFromStorage(fileRef.fullPath);
+        });
+        dir.prefixes.forEach((folderRef) => {
+          deleteFolderFromStorage(folderRef.fullPath);
+        });
+      })
+      .then(() => {
+        removeFromList(fileList, folderPath, "folder");
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return { deleteFileFromStorage, deleteFolderFromStorage };
+}
+
+function removeFromList(fileList, filePath, fileType) {
+  fileList.value = fileList.value.filter((el) => {
+    return el.path !== filePath || el.type !== fileType;
+  });
+}
