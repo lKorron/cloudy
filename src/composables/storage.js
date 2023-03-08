e08cc89e5fc4f171ee02acf6fe0a78590ec72e0d
Korron <lkorron@yandex.ru>
2023-03-08 15:57:41 +0300

    download extraction

--- src/composables/storage.js
+++ src/composables/storage.js
@@ -1,17 +1,15 @@
 import { ref, computed } from "vue";
 import {
   getStorage,
-  getDownloadURL,
   ref as fref,
   uploadBytes,
   uploadString,
   deleteObject,
-  getBlob,
   listAll,
 } from "firebase/storage";
-import { saveAs } from "file-saver";
-import JSZip from "jszip";
+
 import { useFileManager } from "./fileManager";
+import { useDownload } from "./download";
 
 export function useStorage() {
   const fileList = ref([]);
@@ -81,73 +79,14 @@ export function useStorage() {
     }
   };
 
-  const downloadFromStorage = (fileName, filePath, fileType) => {
-    switch (fileType) {
-      case "document":
-        downloadFile(fileName, filePath);
-        break;
-      case "folder":
-        downloadFolderAsZip(filePath);
-        break;
-
-      default:
-        break;
-    }
-  };
-
-  const downloadFile = (fileName, filePath) => {
-    const fileRef = fref(storage, filePath);
-    getDownloadURL(fileRef).then((url) => {
-      fetch(url)
-        .then((response) => response.blob())
-        .then((blob) => {
-          saveAs(blob, fileName);
-        });
-    });
-  };
-
-  const downloadFolderAsZip = async (directoryPath = "") => {
-    const zip = new JSZip();
-
-    await addFilesFromDirectoryToZip(directoryPath, zip);
-
-    if (Object.keys(zip.files).length > 0) {
-      const blob = await zip.generateAsync({ type: "blob" });
-      const name = directoryPath.split("/").pop();
-
-      saveAs(blob, name);
-    }
-  };
-
-  const addFilesFromDirectoryToZip = async (directoryPath = "", zip) => {
-    const storage = getStorage();
-
-    const directoryContentsRef = fref(storage, directoryPath);
-
-    const directoryContents = await listAll(directoryContentsRef);
-
-    for (const file of directoryContents.items) {
-      if (file.name === ".ghostfile") {
-        continue;
-      }
-
-      const fileRef = fref(storage, file.fullPath);
-      const fileBlob = await getBlob(fileRef);
-
-      zip.file(generatePath(file.fullPath, currentPath.value), fileBlob);
-    }
-
-    for (const folder of directoryContents.prefixes) {
-      await addFilesFromDirectoryToZip(folder.fullPath, zip);
-    }
-  };
-
   const { openStorageFolder } = useFileManager(
     storage,
     fileList,
     updateList,
     currentPath
   );
+
+  const { downloadFromStorage } = useDownload(storage, currentPath);
 
   return {
     fileList,
@@ -204,20 +143,4 @@ function isContainsElement(fileList, fileName, fileType) {
       return el.name === fileName && el.type == fileType;
     }).length <= 0
   );
-}
-
-function generatePath(path, initialPath) {
-  let shiftCount = 0;
-  const initialPathArray = initialPath.split("/");
-
-  if (initialPath !== "") {
-    shiftCount = initialPathArray.length;
-  }
-
-  const pathArray = path.split("/");
-  const resultingArray = pathArray.splice(shiftCount);
-
-  const resultingPath = resultingArray.join("/");
-
-  return resultingPath;
 }
