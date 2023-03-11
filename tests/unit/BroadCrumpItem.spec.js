4f1e5f5d9963e7465e510d1802463db46b928321
Korron <lkorron@yandex.ru>
2023-03-11 19:18:34 +0300

    item test refactor

--- tests/unit/BroadCrumpItem.spec.js
+++ tests/unit/BroadCrumpItem.spec.js
@@ -2,38 +2,33 @@ import { mount } from "@vue/test-utils";
 import BroadCrumpItem from "@/components/BroadCrumpItem";
 
 describe("BroadCrumpItem.vue", () => {
-  it("renders value from prop", () => {
-    const testValue = "testValue";
+  let name;
+  let wrapper;
 
-    const wrapper = mount(BroadCrumpItem, {
-      props: {
-        value: testValue,
-      },
+  beforeEach(() => {
+    name = "name";
+
+    wrapper = mount(BroadCrumpItem, {
+      props: { value: name, last: true },
     });
+  });
 
+  it("renders value from prop", () => {
     const item = wrapper.get("[data-test='item']");
 
-    expect(item.text()).toBe(testValue);
+    expect(item.text()).toBe(name);
   });
 
-  it("do not render /, if component last", () => {
-    const wrapper = mount(BroadCrumpItem, {
-      props: {
-        last: true,
-      },
-    });
-    const slash = wrapper.find("[data-test='slash']");
-
+  it("do not render /, if component last", async () => {
+    let slash = wrapper.find("[data-test='slash']");
     expect(slash.exists()).toBe(false);
+
+    await wrapper.setProps({ last: false });
+    slash = wrapper.find("[data-test='slash']");
+    expect(slash.exists()).toBe(true);
   });
 
-  it("emit the name", () => {
-    const wrapper = mount(BroadCrumpItem, {
-      props: {
-        value: "name",
-      },
-    });
-
+  it("emit the name on click", () => {
     const item = wrapper.get("[data-test='item']");
 
     item.trigger("click");
@@ -42,6 +37,6 @@ describe("BroadCrumpItem.vue", () => {
     const clickEvent = wrapper.emitted("itemClicked");
 
     expect(clickEvent).toHaveLength(2);
-    expect(clickEvent[0]).toEqual(["name"]);
+    expect(clickEvent[0]).toEqual([name]);
   });
 });
