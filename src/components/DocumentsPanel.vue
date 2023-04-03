8bd9e78624240eade887ac9f655b9b507d267fcc
Korron <lkorron@yandex.ru>
2023-04-03 18:48:14 +0300

    new isMobile function

--- src/components/DocumentsPanel.vue
+++ src/components/DocumentsPanel.vue
@@ -74,19 +74,9 @@ const props = defineProps({
 });
 
 const isMobile = () => {
-  const toMatch = [
-    /Android/i,
-    /webOS/i,
-    /iPhone/i,
-    /iPad/i,
-    /iPod/i,
-    /BlackBerry/i,
-    /Windows Phone/i,
-  ];
+  const touchDevice = "ontouchstart" in document.documentElement;
 
-  return toMatch.some((toMatchItem) => {
-    return navigator.userAgent.match(toMatchItem);
-  });
+  return touchDevice;
 };
 
 const {
